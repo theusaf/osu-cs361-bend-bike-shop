1496b7fa8021f368b65263b97e6e634bfc73318b
theusaf <theusafyt@gmail.com>
2022-11-12 12:33:52 -0800

    refactor: let cargo take responsibility of other details

--- bike.rb
+++ bike.rb
@@ -1,11 +1,39 @@
 # Bike
 
+class Cargo
+
+  MAX_CARGO_ITEMS = 10
+
+  attr_accessor :max_items
+
+  def initialize(max_items = MAX_CARGO_ITEMS)
+    @max_items = max_items
+    @contents = []
+  end
+
+  def add(item)
+    @contents << item
+  end
+
+  def remove(item)
+    @contents.remove(item)
+  end
+
+  def size
+    @contents.size
+  end
+
+  def remaining_capacity
+    @max_items - size
+  end
+
+end
+
 class Bike
 
   STANDARD_WEIGHT = 200 # lbs
-  MAX_CARGO_ITEMS = 10
 
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
+  attr_accessor :id, :color, :price, :weight, :rented
 
   def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
     @id = id
@@ -13,7 +41,7 @@ class Bike
     @price = price
     @weight = weight
     @rented = rented
-    @cargo_contents = []
+    @cargo = Cargo.new
   end
 
   def rent!
@@ -21,19 +49,19 @@ class Bike
   end
 
   def add_cargo(item)
-    self.cargo_contents << item
+    @cargo.add(item)
   end
 
   def remove_cargo(item)
-    self.cargo_contents.remove(item)
+    @cargo.remove(item)
   end
 
   def pannier_capacity
-    MAX_CARGO_ITEMS
+    @cargo.max_items
   end
 
   def pannier_remaining_capacity
-    MAX_CARGO_ITEMS - self.cargo_contents.size
+    @cargo.remaining_capacity
   end
 
 end
