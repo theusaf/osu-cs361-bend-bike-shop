0061b72b0466e23457a4b9f17d38eef1cd1a869b
theusaf <theusafyt@gmail.com>
2022-11-14 11:17:59 -0800

    refactor: remove luggage definition in bike

--- bike.rb
+++ bike.rb
@@ -6,17 +6,13 @@ class Bike
   STANDARD_WEIGHT = 200 # lbs
 
   attr_reader :id, :color, :price, :weight, :luggage
+  attr_writer :luggage
 
   def initialize(options)
     @id = options[:id]
     @color = options[:color]
     @price = options[:price]
     @weight = STANDARD_WEIGHT
-    @luggage = Luggage.new({
-      :capacity => Luggage::DEFAULT_MAX_CAPACITY,
-      :items => options[:extra_items],
-      :bike => self
-    })
   end
 
 end
--- main.rb
+++ main.rb
@@ -8,11 +8,16 @@ items = [:apple, :water, :protein_bar]
 bike = Bike.new({
   :id => 1,
   :color => :pink,
-  :price => 99.99,
-  :extra_items => items
+  :price => 99.99
 })
+luggage = Luggage.new({
+  :capacity => Luggage::DEFAULT_MAX_CAPACITY,
+  :items => items,
+  :bike => bike
+})
+bike.luggage = luggage
 
-rental = Rental.new(bike, bike.luggage)
+rental = Rental.new(bike, luggage)
 
 puts "Total Price: #{rental.price}, Total Weight: #{rental.weight}"
 
